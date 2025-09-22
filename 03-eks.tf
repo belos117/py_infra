b625bdf8a3b9d8b734c85beb3234dc5e826932c7
stormhenkel <belos117@gmail.com>
2025-09-22 16:55:25 +0200

    Modified eks.tf

--- 03-eks.tf
+++ 03-eks.tf
@@ -34,6 +34,9 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
+  cluster_name    = "eks-${terraform.workspace}"
+  cluster_version = "1.29"
+
   enable_cluster_creator_admin_permissions = true
 
   vpc_id     = module.vpc.vpc_id
