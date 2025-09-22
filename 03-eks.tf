05ffeea05f23e74f8eda634e543ede6c1821f860
stormhenkel <belos117@gmail.com>
2025-09-22 17:05:38 +0200

    Modified eks.tf

--- 03-eks.tf
+++ 03-eks.tf
@@ -1,3 +1,5 @@
+# Filter out local zones, which are not currently supported 
+# with managed node groups
 data "aws_availability_zones" "available" {
   filter {
     name   = "opt-in-status"
@@ -6,13 +8,19 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "eks-${terraform.workspace}"
+  cluster_name = "education-eks-${random_string.suffix.result}"
 }
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1"
 
-  name = "eks-${terraform.workspace}"
+  name = "education-vpc"
 
   cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
@@ -35,14 +43,27 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "20.8.5"
 
-name    = local.cluster_name
+  cluster_name    = local.cluster_name
+  cluster_version = "1.29"
 
+  cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
+  }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
 
   eks_managed_node_groups = {
     one = {
@@ -67,6 +88,19 @@ name    = local.cluster_name
   }
 }
 
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
