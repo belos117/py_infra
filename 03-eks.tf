3856804a071cd344879bb6b267dcc9db7c8b4a85
stormhenkel <belos117@gmail.com>
2025-09-22 14:09:50 +0200

    Modified 03-eks.tf to use current module

--- 03-eks.tf
+++ 03-eks.tf
@@ -1,22 +1,40 @@
 module "vpc" {
-  source          = "terraform-aws-modules/vpc/aws"
-  version         = ">= 3.0"
-  name            = "eks-vpc-${terraform.workspace}"
-  cidr            = "10.0.0.0/16"
-  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks-vpc-${terraform.workspace}"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  name            = "eks-cluster-${terraform.workspace}"
-  version         = "21.3.1"
-  vpc_id          = module.vpc.vpc_id
-  eks_managed_node_groups = {
-    default = {
-      desired_capacity = 2
-      instance_type    = "t3.medium"
-    }
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "eks-cluster-${terraform.workspace}"
+  kubernetes_version = "1.33"
+
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  create_auto_mode_iam_resources = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets_id
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
