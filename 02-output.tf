a6a214d401e283347370334371e45c65dc1bc224
stormhenkel <belos117@gmail.com>
2025-09-22 15:12:35 +0200

    Modified Output.tf

--- 02-output.tf
+++ 02-output.tf
@@ -1,15 +1,5 @@
-output "cluster_name" {
-  description = "The name of the Kubernetes cluster"
-  value       = module.eks.cluster_name
-}
-
 output "kubeconfig" {
   description = "Kubeconfig file content for the cluster"
-  value       = module.eks.kube_config_raw
+  value       = module.eks.kubeconfig
   sensitive   = true
-}
-
-output "oidc_provider_url" {
-  description = "OIDC provider URL for the cluster"
-  value       = module.eks.oidc_issuer_url
 }
